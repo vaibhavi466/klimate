d2e8284378018c422eb9fa022f4e218c0d9f796d
Vaibhavi <vaibhaviagrawal466@gmail.com>
2025-07-04 16:15:27 +0530

    installed tankstack dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,17 @@ import { Layout } from './components/layout';
 import { ThemeProvider } from './context/theme-provider';
 import WeatherDashboard from './pages/weather-dashboard';
 import CityPage from './pages/city-page';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
+
     <BrowserRouter>
     <ThemeProvider defaultTheme='dark'>
       <Layout>
@@ -18,6 +26,8 @@ function App() {
       </Layout>
     </ThemeProvider>
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
