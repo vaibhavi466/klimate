42a7a1d60f17c69b402fe8e89ec4dbeb910cfc12
Vaibhavi <vaibhaviagrawal466@gmail.com>
2025-07-04 16:05:00 +0530

    added sun and moon logo with toggle effect and transition between dark and light theme

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,17 @@
 import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export function Header() {
   const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark"; // ✅ FIXED
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link to="/">
           <img
-            src={theme === "dark" ? "/logo.png" : "/logo2.png"}
+            src={isDark ? "/logo.png" : "/logo2.png"}
             alt="Klimate logo"
             className="h-14 w-auto block"
           />
@@ -17,10 +19,14 @@ export function Header() {
 
         <div className="flex gap-4">
           <div
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-            className="cursor-pointer text-sm text-blue-500"
+            onClick={() => setTheme(isDark ? "light" : "dark")}
+            className={`flex cursor-pointer text-sm transition-transform duration-500 ${isDark ? "rotate-180" : "rotate-0"}`}
           >
-            toggle
+            {isDark ? (
+              <Sun className="h-6 w-6 text-yellow-500 transition-all" />
+            ) : (
+              <Moon className="h-6 w-6 text-blue-500 transition-all" />
+            )}
           </div>
         </div>
       </div>
