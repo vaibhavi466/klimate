ef9069793cfaf972bf99e6100f8702b5eb633e47
Vaibhavi <vaibhaviagrawal466@gmail.com>
2025-07-04 18:56:37 +0530

    feat: update weather dashboard UI with dark mode

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,11 +1,24 @@
+import { Button } from '@/components/ui/button'
+import { RefreshCcw } from 'lucide-react'
 import React from 'react'
 
 const WeatherDashboard = () => {
   return (
     <div>
-      
+      {/* Favourite cities */}
+        <div>
+            <h1>My Location</h1>
+            <Button variant={'outline'}
+             size={"icon"}
+            //  onClick={handleRefresh}
+            // disabled = {}
+            >
+              <RefreshCcw className='h-4 w-4'/>
+            </Button>
+        </div>
+          {/* Current and Hourly weather */}
     </div>
   )
 }
 
-export default WeatherDashboard;
+export default WeatherDashboard
