607e867e1d2cf1ee1686eaddf6dc3d75ca24fa03
Vaibhavi <vaibhaviagrawal466@gmail.com>
2025-07-04 14:16:32 +0530

    set default dark mode to ui using shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 
 import { BrowserRouter } from 'react-router-dom';
 import './App.css'
+import { Layout } from './components/layout';
+import { ThemeProvider } from './context/theme-provider';
 
 function App() {
   return (
     <BrowserRouter>
-    
+    <ThemeProvider defaultTheme='dark'>
+      <Layout>
+        Hello
+      </Layout>
+    </ThemeProvider>
     </BrowserRouter>
   );
 }
