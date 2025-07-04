d2b310bf41c71bdf82f6805faa661ec5ebaa5f36
Vaibhavi <vaibhaviagrawal466@gmail.com>
2025-07-04 14:39:57 +0530

    created header.jsx file

---
+++ src/components/header.tsx
@@ -0,0 +1,6 @@
+const Header = () => {
+  return <header className="py-4 text-center text-xl font-bold">Header</header>;
+};
+
+export default Header; // 
+
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,10 @@
 import type { PropsWithChildren } from "react";
-// import { Header } from "./header";
+import  Header  from "./header";
 
 export function Layout({ children }: PropsWithChildren) {
   return (
     <div className=" bg-gradient-to-br from-background to-muted">
-      {/* <Header /> */}
+      <Header />
       <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
       </main>
