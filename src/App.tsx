2ba2c170de082a8fb7424315bded6cf09a1b8ea4
Vaibhavi <vaibhaviagrawal466@gmail.com>
2025-07-04 14:24:08 +0530

    add routes for dashboard and city

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import { Layout } from './components/layout';
 import { ThemeProvider } from './context/theme-provider';
+import WeatherDashboard from './pages/weather-dashboard';
+import CityPage from './pages/city-page';
 
 function App() {
   return (
     <BrowserRouter>
     <ThemeProvider defaultTheme='dark'>
       <Layout>
-        Hello
+        <Routes>
+          <Route path='/' element={<WeatherDashboard />} />
+          <Route path='/city:cityName' element={<CityPage />} />
+        </Routes>
       </Layout>
     </ThemeProvider>
     </BrowserRouter>
---
+++ src/pages/weather-dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WeatherDashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WeatherDashboard;
